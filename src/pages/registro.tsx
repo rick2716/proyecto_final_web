17448efa28b6d14cb8e87d60fd5b4fc832793522
rick2716 <ricardo.alvarez@uao.edu.co>
2022-05-16 19:24:39 -0500

    Terminar el registro

--- src/pages/registro.tsx
+++ src/pages/registro.tsx
@@ -31,16 +31,25 @@ export default function Registro() {
     [email, passwords]
   );
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!canRegister) return;
+    localStorage.setItem(email[0], passwords[0]);
+  };
+
   return (
     <div className="Container">
       <div className="Header">
-        <h1>Registro</h1>
-        <p>Completa todos los campos para continuar</p>
+        <h1>IMAGEEEEEEEEEEN</h1>
       </div>
       <div className="Body">
+        <div className="Title">
+          <h1>Registro</h1>
+          <p>Completa todos los campos para continuar</p>
+        </div>
         <div className="Formulario">
-          <form>
-            <div>
+          <form onSubmit={handleSubmit}>
+            <div className="InputForm">
               <label htmlFor="correo">Correo </label> <br />
               <input
                 type="text"
@@ -49,7 +58,7 @@ export default function Registro() {
                 onInput={(e) => handleInputEmail(e, 0)}
               />
             </div>
-            <div>
+            <div className="InputForm">
               <label htmlFor="password">Contraseña </label> <br />
               <input
                 type="password"
@@ -58,7 +67,7 @@ export default function Registro() {
                 onInput={(e) => handleInputPassword(e, 0)}
               />
             </div>
-            <div>
+            <div className="InputForm">
               <label htmlFor="passwordCheck">Confirmar Contraseña </label>
               <br />
               <input
@@ -67,6 +76,10 @@ export default function Registro() {
                 value={passwords[1]}
                 onInput={(e) => handleInputPassword(e, 1)}
               />
+              <p id="TextCheck">
+                *Las dos contraseñas tienen que <br />
+                coincidir para poder continuar
+              </p>
             </div>
             <br />
             <div>
